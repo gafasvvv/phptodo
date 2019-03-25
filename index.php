561041e04df8c438794b013813ddba9e6cdf5a56
杉原良輔 <r_sugihara@RYOSUKESUGIHARAnoiMac.local>
2019-03-25 16:32:40 +0900

    削除機能追加

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
     require_once 'htmlspecialchars.php';
     require_once 'Controller.php';
+    //新規作成
     $controller = new Controller;
     $controller->create();
 ?>
@@ -9,6 +10,7 @@
 <head>
     <meta charset="UTF-8">
     <title>To Do リスト</title>
+    <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
 
@@ -24,9 +26,9 @@
             <label for="content">メモ</label>
         </div>
         <div>
-            <textarea name="content" id="content" cols="30" rows="3"></textarea>
+            <input name="content" id="content">
         </div>
-        <input type="submit" value="追加">
+        <input type="submit" name="submit" value="追加">
     </form>
 
     <h2>タスク一覧</h2>
@@ -37,18 +39,23 @@
             <th>メモ</th>
         </tr>
         <?php
+            //削除
+            $controller->delete();
+            //一覧表示
             $stt = $controller->index();
             foreach($stt as $row){
         ?>
         <tr>
-            <td><input type="checkbox"></td>
-            <td><?php echo e($row['name']) ?></td>
-            <td><?php echo e($row['content']) ?></td>
-            <td>
-                <form method="POST" action="Controller.php">
-                    <input type="submit" value="削除">
-                </form>
-            </td>
+            <form method="POST" action="">
+                <td><input type="checkbox" name="checkbox" id="option"></td>
+                <td><?php echo e($row['name']) ?></td>
+                <td><?php echo e($row['content']) ?></td>
+                <td>
+                    <input type="hidden" name="delete" value="true">
+                    <input type="hidden" name="id" value="<?=$row['id']?>">
+                    <input type="submit" name="delete" value="削除">
+                </td>
+            </form>
         </tr>
         <?php
             }
